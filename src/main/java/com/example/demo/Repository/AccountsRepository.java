01bbfa1aefcfada08a4d4074895acfb937ac9de0
9Beeee <221082@st.yoshida-g.ac.jp>
2023-09-21 10:55:37 +0900

    importの参照先の変更 snsからdemoへ

--- src/main/java/com/example/demo/Repository/AccountsRepository.java
+++ src/main/java/com/example/demo/Repository/AccountsRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.Repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.example.sns.entity.Accounts;
+import com.example.demo.entity.Accounts;
 
 /** Accountsテーブル:RepositoryImpl */
 public interface AccountsRepository extends CrudRepository<Accounts, Integer> {
